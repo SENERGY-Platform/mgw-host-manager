83fb88f384b381fe6a8bec5e90e127a1d88543bd
y-du <42994541+y-du@users.noreply.github.com>
2023-07-03 13:43:47 +0200

    init

---
+++ handler/itf.go
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2023 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package handler
+
+import (
+	"context"
+	"github.com/SENERGY-Platform/mgw-host-manager/lib/model"
+)
+
+type HostInfoHandler interface {
+	GetNet(ctx context.Context) (model.HostNet, error)
+	GetCPU(ctx context.Context) error
+	GetRAM(ctx context.Context) error
+	GetOS(ctx context.Context) error
+}
