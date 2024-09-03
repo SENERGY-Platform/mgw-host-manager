fa06e48eff26f2d95e1d9790bd7510841a25d844
y-du <42994541+y-du@users.noreply.github.com>
2024-09-03 10:28:05 +0200

    remove mdns endpoint

--- handler/http_hdl/handler.go
+++ handler/http_hdl/handler.go
@@ -31,9 +31,6 @@ const (
 type hostResourcesQuery struct {
 }
 
-type serviceGroupQuery struct {
-}
-
 func getHostInfoH(a lib.Api) gin.HandlerFunc {
 	return func(gc *gin.Context) {
 		hostInfo, err := a.GetHostInfo(gc.Request.Context())
@@ -80,79 +77,6 @@ func getHostResourceH(a lib.Api) gin.HandlerFunc {
 			return
 		}
 		gc.JSON(http.StatusOK, resource)
-	}
-}
-
-func getMDNSAdvListH(a lib.Api) gin.HandlerFunc {
-	return func(gc *gin.Context) {
-		query := serviceGroupQuery{}
-		if err := gc.ShouldBindQuery(&query); err != nil {
-			_ = gc.Error(model.NewInvalidInputError(err))
-			return
-		}
-		advList, err := a.ListMDNSAdv(gc.Request.Context(), model.ServiceGroupFilter{})
-		if err != nil {
-			_ = gc.Error(err)
-			return
-		}
-		gc.JSON(http.StatusOK, advList)
-	}
-}
-
-func getMDNSAdvH(a lib.Api) gin.HandlerFunc {
-	return func(gc *gin.Context) {
-		adv, err := a.GetMDNSAdv(gc.Request.Context(), gc.Param(mDNSAdvParam))
-		if err != nil {
-			_ = gc.Error(err)
-			return
-		}
-		gc.JSON(http.StatusOK, adv)
-	}
-}
-
-func putMDNSAdvAddH(a lib.Api) gin.HandlerFunc {
-	return func(gc *gin.Context) {
-		var sg model.ServiceGroup
-		err := gc.ShouldBindJSON(&sg)
-		if err != nil {
-			_ = gc.Error(model.NewInvalidInputError(err))
-			return
-		}
-		err = a.AddMDNSAdv(gc.Request.Context(), sg)
-		if err != nil {
-			_ = gc.Error(err)
-			return
-		}
-		gc.Status(http.StatusOK)
-	}
-}
-
-func putMDNSAdvUptH(a lib.Api) gin.HandlerFunc {
-	return func(gc *gin.Context) {
-		var sg model.ServiceGroup
-		err := gc.ShouldBindJSON(&sg)
-		if err != nil {
-			_ = gc.Error(model.NewInvalidInputError(err))
-			return
-		}
-		sg.ID = gc.Param(mDNSAdvParam)
-		err = a.UpdateMDNSAdv(gc.Request.Context(), sg)
-		if err != nil {
-			_ = gc.Error(err)
-			return
-		}
-		gc.Status(http.StatusOK)
-	}
-}
-
-func deleteMDNSAdvH(a lib.Api) gin.HandlerFunc {
-	return func(gc *gin.Context) {
-		err := a.DeleteMDNSAdv(gc.Request.Context(), gc.Param(mDNSAdvParam))
-		if err != nil {
-			_ = gc.Error(err)
-			return
-		}
-		gc.Status(http.StatusOK)
 	}
 }
 
